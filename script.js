3fc6a262b50b1b53f7ff0626f6ace8e92663f12d
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-24 16:33:49 +0300

    add new tıme element

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// JavaScript source code
+var wakeupTime = 7;
+var noon = 12;
+var lunchtime = 12;
+var parttime;
+var evening;
+
+var showCurrentTime = function{
+    var clock = document.getElementById("clock");
+
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var meridian = "AM";
+}
